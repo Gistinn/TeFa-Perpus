80d3cd3dae131b76ee135d13f501bc9068cebda1
Agistinaa <142241111+Gistinn@users.noreply.github.com>
2024-02-29 08:56:47 +0700

    Add files via upload

---
+++ buku/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container-fluid">
+   <div class="row my-5">
+    <div class="col-lg-6">
+      <nuxt-link to="/pengunjung/tambah">
+        <div class="card bg-pengunjung rounded-5">
+          <div class="card-body">
+            <h2>pengunjung</h2>
+
+          </div>
+        </div>
+      </nuxt-link>
+    </div>
+    
+    <div class="col-lg-6">
+      <nuxt-link to="/buku">
+        <div class="card bg-buku rounded-5">
+          <div class="card-body">
+            <h2>cari Buku</h2>
+          </div>
+        </div>
+      </nuxt-link>
+    </div>
+   </div>
+  </div>
+</template>
+
+
+<style scoped>
+ .card {
+   height: 250px;
+   box-shadow: 1px 1px 10px #818181
+ }
+ .card.bg-pengunjung {
+  background-image: url('../asset/img/bg-home-kunjungan.jpeg');
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+ }
+ .card.bg-buku {
+  background: url('../asset/img/bg-home-cari-buku.jpg') no-repeat center center;
+  background-size: cover;
+ }
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="row p-3 bg-primary text-white">
+    <div class="col-lg-1">
+      <img src="~/assets/img/LOGO-SMK4.png" alt="logo smk4">
+    </div>
+
+    <div class="col-lg-11">
+      <h1>PERPUS DIGITAL</h1>
+      <adress>
+        SMKN 4 TASIKMALAYA
+        JL.DEPOK , SUKAMENAK , PURBARATU
+      </adress>
+    </div>
+  </div>
+</template>
+<style scoped>
+.logo{
+  width: 100%;
+}
+</style>
