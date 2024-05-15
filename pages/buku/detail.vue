3b2afff8d53c4a1d00a57de3c0ba1bed03b75778
Agistinaa <142241111+Gistinn@users.noreply.github.com>
2024-05-15 15:23:31 +0700

    Add files via upload

---
+++ pages/buku/detail.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container shadow p-2 mt-3">
+        <div class="row mt-4">
+            <div class="col-4">
+                    <img src="assets/assets/img/172_days.jpg" class="cover" alt="cover 2" height="340px" width="340px">
+                </div>
+
+            <div class="col-4">
+
+<p>Informasi lain :</p>
+
+<p>Judul : 172 Days</p>
+
+
+<p>Penulis : Nadzira Shafa </p>
+<p>Penerbit : Motivaksi Inspira </p>
+<p>Tanggal terbit : 20 juli 2022</p>
+</div>
+</div>
+        <div class="row">
+            <p class="mt-5">Novel  172 Days mengikuti kisah nyata tentang sosok Zira, gadis muda yang dilahirkan dan dibesarkan di lingkungan agamis.
+kan tetapi, latar belakang keluarga agamis tak lantas membuat Zira hidup dalam jalan yang selalu lurus.
+Akibat suatu kejadian di sekolah, Zira mulai mengikuti pergaulan yang salah dan buruk dalam pandangan agama. Keputusan Zira untuk hijrah akhirnya datang setelah ada masalah di lingkup keluarganya. 
+Zira mulai mengikuti kegiatan religi sebagai bagian dari proses hijrah, seperti pengajian.</p>
+        </div>
+        <nuxt-link to="http://localhost:3000/buku">
+                    <button type="submit" class="btn btn-dark btn-lg rounded-5 px-5">kembali</button>
+                </nuxt-link>
+        </div>
+</template>
+<style scoped>
+</style>
---
+++ pages/buku/index.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="text-center my-4">PILIHLAH BUKU YANG INGIN ANDA BACA</h2>
+                <div class="my-3">
+                    <input type="search" class="form-contol rounded-5" placeholder="mau baca apa hari ini ?">
+                </div>
+                <div class="my-3 text-muted">menampilkan 3 dari 3</div>
+                <div class="row">
+                    <div class="col-lg-2">
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <nuxt-link to="rincian-buku/">
+                                <img src="~/assets/img/172_Days.jpg" class="cover" alt="cover 1">
+                                </nuxt-link>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-2">
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <img src="~/assets/img/ancika.jpg" class="cover" alt="cover 2">   
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-2">
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <img src="~/assets/img/mariposa(2).jpg" class="cover" alt="cover 3">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <nuxt-link to="http://localhost:3000/">
+                    <button type="submit" class="btn btn-dark btn-lg rounded-5 px-5">kembali</button>
+                </nuxt-link>
+    </div>
+</template>
+
+
+<style scoped>
+
+.cover {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: 0 30;
+}
+</style>
+
+<script setup>
+const supabase = useSupabaseClient()
+
+const books = ref([])
+
+const getBooks = async () => {
+    if(data) books.value = data
+}
+
+onMounted(() => {
+    getBooks()
+})
+</script>
